70a651df25037747b7177c0f306881e557094daf
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-09-22 17:21:23 +0530

    Fixed mentionall 🛠

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -8,12 +8,12 @@ let Lang = Language.getString('admin');
 async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
     var sonuc = grup['participants'].map((member) => {
-        if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
+        if (member.jid.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
     });
     return sonuc.includes(true);
 }
 
-WhatsAlexa.addCommand({pattern: 'mentionall', fromMe: true, desc: TLang.TAGALL_DESC}, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
 
