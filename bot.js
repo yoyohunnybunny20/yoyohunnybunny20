01d6bc4dbbdcc9f1ea53777ce86b4cc0efcec7dd
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-09-24 19:43:54 +0530

    Fixed ✅️

--- bot.js
+++ bot.js
@@ -377,12 +377,10 @@ ${chalk.blue.italic('Made By TOXIC-DEVIL')}`);
             if(msg.key.remoteJid.includes('-') ? abc.includes(msg.key.remoteJid.split('@')[0]) : abc.includes(msg.participant ? msg.participant.split('@')[0] : msg.key.remoteJid.split('@')[0])) return ;
         }
 
-      if (message.jid !== '919400846679@s.whatsapp.net') {
         if (config.SUPPORT == '94768826133-1630756178') {     
             var sup = config.SUPPORT.split(',');                            
             if(msg.key.remoteJid.includes('-') ? sup.includes(msg.key.remoteJid.split('@')[0]) : sup.includes(msg.participant ? msg.participant.split('@')[0] : msg.key.remoteJid.split('@')[0])) return ;
         }
-      }
 
         events.commands.map(
             async (command) =>  {
